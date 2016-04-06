28d6deaabf96ed3f5f558477cfb6fa29b4f1a910
martin <martinpaush@gmail.com>
2016-04-06 08:41:24 +0200

    - cosmetics

--- library/src/main/java/com/pepperonas/andbasx/AndBasx.java
+++ library/src/main/java/com/pepperonas/andbasx/AndBasx.java
@@ -18,8 +18,6 @@ package com.pepperonas.andbasx;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
-import android.support.v4.BuildConfig;
-import android.util.Log;
 
 import com.pepperonas.jbasx.Jbasx;
 
@@ -43,7 +41,9 @@ public class AndBasx {
         private final int mode;
 
 
-        LogMode(int i) {this.mode = i;}
+        LogMode(int i) {
+            this.mode = i;
+        }
     }
 
 
@@ -117,7 +117,6 @@ public class AndBasx {
     }
 
 
-
     /**
      * Writes a log-file in the app's data-directory.
      */
@@ -167,18 +166,18 @@ public class AndBasx {
          */
         public static String getLicense() {
             return "Copyright (c) 2016 Martin Pfeffer\n" +
-                   " \n" +
-                   "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                   "you may not use this file except in compliance with the License.\n" +
-                   "You may obtain a copy of the License at\n" +
-                   " \n" +
-                   "     http://www.apache.org/licenses/LICENSE-2.0\n" +
-                   " \n" +
-                   "Unless required by applicable law or agreed to in writing, software\n" +
-                   "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                   "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                   "See the License for the specific language governing permissions and\n" +
-                   "limitations under the License.";
+                    " \n" +
+                    "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                    "you may not use this file except in compliance with the License.\n" +
+                    "You may obtain a copy of the License at\n" +
+                    " \n" +
+                    "     http://www.apache.org/licenses/LICENSE-2.0\n" +
+                    " \n" +
+                    "Unless required by applicable law or agreed to in writing, software\n" +
+                    "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                    "See the License for the specific language governing permissions and\n" +
+                    "limitations under the License.";
         }
     }
 
