4ab257d9154b8806dcc75a4145b7375f23fc59fc
martin <martinpaush@gmail.com>
2016-06-10 15:38:36 +0200

    - added ViewUtils

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Add JitPack to your repositories:
 and add the library to your dependencies:
 
 ```
-    compile 'com.github.pepperonas:andbasx:0.2.1'
+    compile 'com.github.pepperonas:andbasx:0.2.2'
 ```
 
 This library may require the following permissions:
---
+++ library/src/main/java/com/pepperonas/andbasx/gui/ViewUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Martin Pfeffer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pepperonas.andbasx.gui;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * The type View utils.
+ *
+ * @author Martin Pfeffer (pepperonas)
+ */
+public class ViewUtils {
+
+    /**
+     * Gets all children.
+     *
+     * @param v the v
+     * @return the all children
+     */
+    public static ArrayList<View> getAllChildren(View v) {
+
+        if (!(v instanceof ViewGroup)) {
+            ArrayList<View> viewArrayList = new ArrayList<>();
+            viewArrayList.add(v);
+            return viewArrayList;
+        }
+
+        ArrayList<View> result = new ArrayList<>();
+
+        ViewGroup vg = (ViewGroup) v;
+        for (int i = 0; i < vg.getChildCount(); i++) {
+
+            View child = vg.getChildAt(i);
+
+            ArrayList<View> viewArrayList = new ArrayList<>();
+            viewArrayList.add(v);
+            viewArrayList.addAll(getAllChildren(child));
+
+            result.addAll(viewArrayList);
+        }
+        return result;
+    }
+
+}
